6bb0a4e5007f062867eb3aaa72c6ae317bb4ab86
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-11-10 15:12:51 +0300

    commit_1

--- certification_1/Program.cs
+++ certification_1/Program.cs
@@ -1,43 +1,16 @@
-﻿﻿Console.Write("Введите количесво элементов массива: ");
+﻿Console.Write("Введите количесво элементов массива: ");
 int size = int.Parse(Console.ReadLine()!);
-//Random rand = new Random();
-//Console.Write("[ ");
-/*int[] myArray(int s)      //заполнение массива случайными числами
-{
-    int[] array = new int[s];
-    for (int i = 0; i < s; i++)
-    {
-        array[i] = rand.Next(1, 99);
-        Console.Write($"{array[i]} ");
-    }
-    return array;
-}*/
 
-/*int maxfunction (int[] myArray) //нахождение максимального элемента массива
+string[] str_array(int s)        //заполнение массива вводом с консоли
 {
-    int max = myArray[0];
-    for (int i = 1; i < myArray.Length; i++)
+    int i;
+    string[] str1 = new string[s];
+    for (i = 0; i < s; i++)
     {
-        if (max < myArray[i]) max = myArray[i];
+        Console.Write($"Введите элемент массива [{i}]:  ");
+        str1[i] = Console.ReadLine();
     }
-    return max;
-}*/
+    return str1;
+}
 
-/*int minfunction (int[] myArray) //нахождение минимального элемента массива
-{
-    int min = myArray[0];
-    for (int i = 1; i < myArray.Length; i++)
-    {
-        if (min > myArray[i]) min = myArray[i];
-    }
-    return min;
-}*/
-
-/*int[] myArr = myArray(size);
-Console.WriteLine("]");
-int max = maxfunction(myArr);
-Console.WriteLine($"Максимальное значение элемента массива: {max}");
-int min = minfunction(myArr);
-Console.WriteLine($"Минимальное значение элемента массива: {min}");
-int result = max - min;
-Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {result}");*/
+string[] str = str_array(size);
