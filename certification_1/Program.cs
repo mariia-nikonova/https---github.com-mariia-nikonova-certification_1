8fb8950aad24ac8676bcf30dd85504287a211169
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-11-12 11:34:56 +0300

    commit_2

--- certification_1/Program.cs
+++ certification_1/Program.cs
@@ -1,43 +1,31 @@
-﻿﻿Console.Write("Введите количесво элементов массива: ");
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+Console.Write("Введите количесво элементов массива: ");
 int size = int.Parse(Console.ReadLine()!);
-//Random rand = new Random();
-//Console.Write("[ ");
-/*int[] myArray(int s)      //заполнение массива случайными числами
+
+string[] str_array(int s)        //заполнение массива вводом с консоли
 {
-    int[] array = new int[s];
+    int i;
+    string[] str1 = new string[s];
+    for (i = 0; i < s; i++)
+    {
+        Console.Write($"Введите элемент массива [{i}]:  ");
+        str1[i] = Console.ReadLine();
+    }
+    return str1;
+}
+
+void print (string[] array)      //печать массива
+{
+    Console.WriteLine();
+    int s=array.Length;
+    Console.WriteLine("Исходный массив строк:");
     for (int i = 0; i < s; i++)
     {
-        array[i] = rand.Next(1, 99);
-        Console.Write($"{array[i]} ");
+        Console.Write($"{array[i]}  ");
     }
-    return array;
-}*/
+}
 
-/*int maxfunction (int[] myArray) //нахождение максимального элемента массива
-{
-    int max = myArray[0];
-    for (int i = 1; i < myArray.Length; i++)
-    {
-        if (max < myArray[i]) max = myArray[i];
-    }
-    return max;
-}*/
-
-/*int minfunction (int[] myArray) //нахождение минимального элемента массива
-{
-    int min = myArray[0];
-    for (int i = 1; i < myArray.Length; i++)
-    {
-        if (min > myArray[i]) min = myArray[i];
-    }
-    return min;
-}*/
-
-/*int[] myArr = myArray(size);
-Console.WriteLine("]");
-int max = maxfunction(myArr);
-Console.WriteLine($"Максимальное значение элемента массива: {max}");
-int min = minfunction(myArr);
-Console.WriteLine($"Минимальное значение элемента массива: {min}");
-int result = max - min;
-Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {result}");*/
+string[] str = str_array(size);
+print(str);
